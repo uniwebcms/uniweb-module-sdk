44773df5c3525b7b8862d70357736688a77428f8
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-05-18 16:05:17 -0400

    method stripHTMLTags

---
+++ src/helper.js
@@ -0,0 +1,16 @@
+/**
+ * function that uses regular expressions to remove HTML tags from an HTML string and returns a plain string
+ * @param {string} htmlString
+ * @returns
+ */
+function stripHTMLTags(htmlString) {
+    // Remove HTML tags using regular expression
+    const plainString = htmlString.replace(/<[^>]*>/g, '');
+
+    // Decode HTML entities
+    const decodedString = new DOMParser().parseFromString(plainString, 'text/html').body.textContent;
+
+    return decodedString;
+}
+
+export { stripHTMLTags };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import PopoverMenu from './PopoverMenu';
 import SmartLink from './SmartLink';
 import DocumentImage from './DocumentImage';
 import Blogs from './Blogs';
+import { stripHTMLTags } from './helper';
 
 const website = uniweb.activeWebsite;
 
@@ -11,3 +12,5 @@ const { client, postClient, localize, getProfile, getProfileSection, getListProf
 export { client, postClient, localize, getProfile, getProfileSection, getListProfileItems, getProfileTypes, findLink, queryFromContent, parseLinksInArticle, getFirstElementInMap, Profile, website };
 
 export { ProfileImage, PopoverMenu, SmartLink, DocumentImage, Blogs };
+
+export { stripHTMLTags };
