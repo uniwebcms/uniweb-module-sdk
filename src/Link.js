efc478a9dcf41b772f95c88bd790a325e5c9f235
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-06-05 11:20:31 -0400

    fix

--- src/Link.js
+++ src/Link.js
@@ -1,8 +1,7 @@
 import React, { lazy, Suspense } from 'react';
-import { website } from './index.js';
 
 const Component = lazy(() =>
-    website.getComponent('components', 'SimpleLink').catch(() => ({ default: () => null }))
+    uniweb.getComponent('components', 'SimpleLink').catch(() => ({ default: () => null }))
 );
 
 export default function Link(props) {
@@ -12,74 +11,3 @@ export default function Link(props) {
         </Suspense>
     );
 }
-
-// const Link = lazy(() =>
-//     website
-//         .getComponent('components', 'SmartLink')
-//         .then((module) => ({ default: module.Link }))
-//         .catch(() => ({ default: () => null }))
-// );
-
-// export default (props) => {
-//     const {
-//         children,
-//         target = '_self',
-//         title = '',
-//         style = null,
-//         className = '',
-//         to = '',
-//         profile = null,
-//         ariaLabel = '',
-//         options = {}
-//     } = props;
-
-//     let anchorProps = {
-//         className,
-//         style
-//     };
-
-//     if (ariaLabel) {
-//         anchorProps['aria-label'] = ariaLabel;
-//     }
-
-//     if (title) anchorProps.title = title;
-
-//     let href = to;
-
-//     if (profile) {
-//         href = profile.makeProfileHref(options);
-//     }
-
-//     const domainsInfo = uniweb.activeWebsite.getDomainsInfo();
-//     const base_route = domainsInfo?.base_route;
-
-//     // Link from website to uniweb
-//     if (base_route && href.indexOf('/') === 0) {
-//         let appDomain = domainsInfo?.appDomain;
-//         href = `${appDomain}${href}`;
-//     }
-
-//     const externalLink = href.includes('//');
-
-//     if (externalLink) {
-//         anchorProps = {
-//             ...anchorProps,
-//             target,
-//             href
-//         };
-
-//         return <a {...anchorProps}>{children}</a>;
-//     } else {
-//         anchorProps = {
-//             ...anchorProps,
-//             target,
-//             to: href
-//         };
-//     }
-
-//     return (
-//         <Suspense fallback={''}>
-//             <Link {...anchorProps}>{children}</Link>
-//         </Suspense>
-//     );
-// };
