131e47b425ba6f5fde976cb341fdf16356c272be
tianyu <tianyu@proximify.ca>
2023-06-12 23:54:07 +0800

    Prepare the asset;

--- src/DocumentImage.js
+++ src/DocumentImage.js
@@ -1,48 +1,22 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Profile } from './helper';
+import React from 'react';
+// import { Profile } from './helper';
 
 const Image = (props) => {
     const {
-        contentType = 'docufolio',
-        contentId,
+        // contentType = 'docufolio',
+        // contentId,
+        profile,
         value,
         alt = '',
         className = 'w-full h-full object-cover',
-        filePreview = false,
         externalSrc = ''
     } = props;
 
-    const profile = Profile.newProfile(contentType, contentId);
+    // const profile = Profile.newProfile(contentType, contentId);
 
-    const { href, src, altText } = profile.getAssetInfo(value, filePreview, alt);
+    const { src, altText } = profile.getAssetInfo(value, true, alt);
 
-    const [imgSrc, setImgSrc] = useState(src);
-    const [isFallback, setIsFallback] = useState(false);
-
-    useEffect(() => {
-        setImgSrc(src);
-        setIsFallback(false);
-    }, [src]);
-
-    const fetchImgSrc = useCallback(() => {
-        fetch(href)
-            .then((res) => res.json())
-            .then((res) => {
-                setIsFallback(true);
-                setImgSrc(res);
-            });
-    }, [href]);
-
-    return (
-        <img
-            src={externalSrc || imgSrc}
-            className={className}
-            alt={altText}
-            onError={(event) => {
-                if (Profile.editableMode() && !isFallback) fetchImgSrc();
-            }}
-        />
-    );
+    return <img src={externalSrc || src} className={className} alt={altText} />;
 };
 
 export default function DocumentImage(props) {
@@ -52,22 +26,3 @@ export default function DocumentImage(props) {
 
     return <Image {...props}></Image>;
 }
-
-/**
- * 
-    // let finalType = contentType === 'resources' ? 'equipment' : contentType;
-
-    // const [version, identifier, filename] = value.split('/');
-
-    // const ext = filename.split('.').pop();
-
-    // const previewMode = filePreview && !['jpg', 'jpeg', 'png', 'webp', 'gif', 'svg'].includes(ext);
-
-    // const assetRootUrl = website.getAssetRootUrl();
-
-    // const src = `${assetRootUrl}${finalType}/${viewType}/${contentId}/${previewMode ? `${identifier}_preview` : identifier}_v${version}.${previewMode ? 'webp' : ext}`;
-
-    // const altText = website.localize(alt) || filename;
-
-    // const href = website.buildLoadProfileAssetURL(contentId, contentType, value, viewType, identifier);
- */
