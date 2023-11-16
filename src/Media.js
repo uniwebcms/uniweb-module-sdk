05f5e62af93ed1fa8d62b82feb3869b9f6dadb03
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-11-16 13:46:28 -0500

    media supports facade mode

--- src/Media.js
+++ src/Media.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, createRef } from 'react';
 import Image from './Image';
 import { twMerge } from 'tailwind-merge';
+import { BiPlay } from 'react-icons/bi';
 
 export default function Media(props) {
-    const { profile, media, className = '', style, asBg = false } = props;
+    const { profile, media, className = '', style, asBg = false, thumbnail } = props;
+
+    const frameRef = createRef(null);
 
     if (!media) return null;
 
@@ -17,35 +20,72 @@ export default function Media(props) {
         const isYouTube = src.startsWith('https://www.youtube');
         const isVimeo = src.startsWith('https://player.vimeo.com/');
 
-        if (isYouTube) {
-            return (
-                <div
-                    className={twMerge('relative', className)}
-                    style={style ?? { paddingBottom: '56.25%' }}>
-                    <iframe
-                        className={twMerge('absolute inset-0 w-full h-full', className)}
-                        src={asBg ? `${src}?autoplay=1&mute=1&controls=0&loop=1` : src}
-                        title={caption}
-                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                        allowFullScreen></iframe>
-                </div>
-            );
-        } else if (isVimeo) {
-            return (
-                <div
-                    className={twMerge('relative', className)}
-                    style={style ?? { paddingBottom: '56.25%' }}>
-                    <iframe
-                        className={twMerge('absolute inset-0 w-full h-full', className)}
-                        src={asBg ? `${src}?autoplay=1&muted=1&controls=0&loop=1` : src}
-                        title={caption}
-                        allow='autoplay; fullscreen; picture-in-picture'
-                        allowFullScreen></iframe>
+        const urlParams = asBg
+            ? isYouTube
+                ? '?autoplay=1&mute=1&controls=0&loop=1'
+                : '?background=1'
+            : thumbnail
+            ? '?autoplay=1'
+            : '';
+
+        const allow = isYouTube
+            ? 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            : 'autoplay; fullscreen; picture-in-picture';
+
+        const frame = (
+            <iframe
+                ref={frameRef}
+                className={twMerge('absolute inset-0 w-full h-full', className)}
+                src={`${src}${urlParams}`}
+                title={caption}
+                allow={allow}
+                allowFullScreen></iframe>
+        );
+
+        if (isYouTube || isVimeo) {
+            return asBg ? (
+                <div>{frame}</div>
+            ) : (
+                <div className={twMerge('relative')} style={style ?? { paddingBottom: '56.25%' }}>
+                    {thumbnail ? (
+                        <FacadeVideo profile={profile} thumbnail={thumbnail} className={className}>
+                            {frame}
+                        </FacadeVideo>
+                    ) : (
+                        frame
+                    )}
                 </div>
             );
         } else {
-            console.log('Unable to determine media source type');
+            console.log('Unsupported media type, expected YouTube or Vimeo.');
             return null;
         }
     }
 }
+
+const FacadeVideo = ({ profile, thumbnail, children, className }) => {
+    const [showVideo, setShowVideo] = useState(false);
+
+    const { value, alt, url } = thumbnail;
+
+    if (!showVideo) {
+        return (
+            <>
+                <div className='absolute inset-0 w-full h-full'>
+                    <Image {...{ profile, value, alt, url, className }} />
+                </div>
+                <div
+                    className='absolute inset-0 w-full h-full flex items-center justify-center cursor-pointer group'
+                    onClick={() => {
+                        setShowVideo(true);
+                    }}>
+                    <div className='w-12 h-12 py-2 pl-2.5 pr-1.5 ring-1 !ring-gray-300 rounded-full !bg-white'>
+                        <BiPlay className='w-full h-full !text-indigo-400 group-hover:!text-indigo-700' />
+                    </div>
+                </div>
+            </>
+        );
+    } else {
+        return children;
+    }
+};
