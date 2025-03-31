3b6a7dd98ab22b7e3416370b222bf065240e1cbb
tianyu <tianyu@proximify.ca>
2025-03-31 15:52:02 -0400

    Handle legacy icon; new version of the sdk;

--- src/Icon.js
+++ src/Icon.js
@@ -4,11 +4,20 @@ import UniwebIcon from './UniwebIcon';
 
 export default function (props) {
     // return <SVG className={className} src={icon} aria-hidden='true'></SVG>;
-    //Convert old IconBlock props to new UniwebIcon props
-    if (props.src) {
-        props.svg = props.src;
-        delete props.src;
+
+    let iconProps = { ...props };
+
+    if (props.icon) {
+        let icon = props.icon;
+        //Convert old icon prop to new UniwebIcon props
+        if (typeof icon === 'string') {
+            iconProps.svg = props.icon;
+        } else {
+            iconProps = { ...iconProps, ...icon };
+        }
+
+        delete iconProps.icon;
     }
 
-    return <UniwebIcon {...props} />;
+    return <UniwebIcon {...iconProps} />;
 }
