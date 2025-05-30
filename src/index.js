fc2828a2339da9214873c69e8f72a1fd88b5b308
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2025-05-30 17:39:30 +0800

    remove react code and simplify builder

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-// import Link from './Link';
-// import Image from './Image';
-// import Asset from './Asset';
-// import FileLogo from './FileLogo';
-// import SafeHtml from './SafeHtml';
-// import Icon from './Icon';
-// import Disclaimer from './Disclaimer';
-// import MediaIcon from './MediaIcon';
-// import Media from './Media';
-import {
-    Link,
-    Image,
-    Asset,
-    FileLogo,
-    SafeHtml,
-    Icon,
-    Disclaimer,
-    MediaIcon,
-    Media
-} from '@uniwebcms/core-components';
-
 import {
     Profile,
     website,
@@ -39,9 +18,6 @@ import { twJoin, twMerge } from 'tailwind-merge';
 
 // Methods and objects from the Uniweb engine
 export { Profile, website, getPageProfile, stripTags };
-
-// React components
-export { Link, Image, Asset, Icon, SafeHtml, FileLogo, MediaIcon, Media, Disclaimer };
 
 // Locally defined functions
 export { useBlockState, useGetProfile, useLoadProfileBody, useBlockInputFilterState, getComponent };
