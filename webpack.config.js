9f4040f0baff4150f30a7c05261fc46a7e851183
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2025-05-30 21:08:19 +0800

    put react back

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,45 @@
 const path = require('path');
 
-module.exports = () => ({
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = (_, argv) => {
+    return {
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        resolve: {
+            extensions: ['.jsx', '.js', '.json']
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve('dist'),
+            clean: true,
+            publicPath: '/dist/',
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        resolve: {
+            alias: {
+                react: path.resolve(__dirname, './node_modules/react'),
+                'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+            }
+        },
+        externals: {
+            react: 'react',
+            'react-dom': 'react-dom'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
                     }
                 }
-            }
-        ]
-    }
-});
+            ]
+        },
+        watchOptions: {
+            ignored: ['**/node_modules']
+        }
+    };
+};
